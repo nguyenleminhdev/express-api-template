f3c91f9e4fbec09af8a294790eed15bcd2b50305
Thien Minh <nguyenleminhdev@gmail.com>
2020-12-29 10:59:54 +0700

    update

--- server/core/body-parser.js
+++ server/core/body-parser.js
@@ -9,8 +9,8 @@ const body_parser = require('body-parser')
 
 
 module.exports = proceed => {
-    App.use(body_parser.urlencoded({ extended: false }))
     App.use(body_parser.json())
+    App.use(body_parser.urlencoded({ extended: true }))
 
     console.log('=> Parser json body successfully')
     proceed()
